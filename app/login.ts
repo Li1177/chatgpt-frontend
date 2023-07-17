889793f0ba81a45e50b559f707ee57cc03185e1f
Li1177 <110624605+Li1177@users.noreply.github.com>
2023-07-18 00:52:09 +0800

    Update login.ts

--- app/login.ts
+++ app/login.ts
@@ -1,38 +1,45 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
+
 import { Form, Input, Button } from 'antd';
+
 import RegisterPage from './register';
 
 const LoginPage = () => {
-  const [phone, setPhone] = useState('');
-  const [code, setCode] = useState('');
 
-  const handleLogin = () => {
-    // 调用部署在ngrok上的后端登录接口
-    fetch('https://f4f8-114-246-218-115.ngrok-free.app/login', {
+  const (phone, setPhone) = useState('');
+
+  const (code, setCode) = useState('');
+
+  const handleLogin = async () => {
+
+    const res = await fetch('https://xxx-ngrok-url.io/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         phone,
-        code
+        code  
       })
-    }).then(res => {
-      console.log('登录成功');
     });
+
+    const data = await res.json();
+
+    console.log('登录成功');
+
   };
 
   return (
     <>
       <Form>
-        {/* 登录表单 */}
+       // 登录表单
       </Form>
-
+      
       <div>
-        还没有账号？<RegisterPage />
+        还没有账号? <RegisterPage />
       </div>
     </>
   );
-};
+}
 
 export default LoginPage;
